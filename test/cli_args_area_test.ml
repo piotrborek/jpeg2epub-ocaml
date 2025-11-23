f036e7774f0b3fee4778f048658d162343c350c0
Piotr Borek <piotrborek@op.pl>
2025-11-23 16:00:59 +0100

    Added some tests.

--- test/cli_args_area_test.ml
+++ test/cli_args_area_test.ml
@@ -3,10 +3,43 @@ open Jpeg2epub.Cli_args
 let area = Alcotest.testable Area.pp Area.equal
 
 let when_empty_string_should_return_none () =
-    Alcotest.(check @@ option area) "returned none" (Area.create_from_string "") None
+    Alcotest.(check @@ option area) "returned none" None (Area.create_from_string "")
+
+let when_wrong_string_1_should_return_none () =
+    Alcotest.(check @@ option area) "returned none" None (Area.create_from_string "test")
+
+let when_wrong_string_2_should_return_none () =
+    Alcotest.(check @@ option area) "returned none" None (Area.create_from_string "a b c d")
+
+let when_wrong_string_3_should_return_none () =
+    Alcotest.(check @@ option area) "returned none" None (Area.create_from_string "a b c")
+
+let when_wrong_string_4_should_return_none () =
+    Alcotest.(check @@ option area) "returned none" None (Area.create_from_string "1")
+
+let when_wrong_string_5_should_return_none () =
+    Alcotest.(check @@ option area) "returned none" None (Area.create_from_string "1 2")
+
+let when_wrong_string_6_should_return_none () =
+    Alcotest.(check @@ option area) "returned none" None (Area.create_from_string "1 2 3")
+
+let when_wrong_string_7_should_return_none () =
+    Alcotest.(check @@ option area) "returned none" None (Area.create_from_string "1 2 3 4 5")
+
+let when_string_should_return_area () =
+    let expected = Some {Area.x = 1; y = 2; width = 3; height = 4} in
+    Alcotest.(check @@ option area) "returned none" expected (Area.create_from_string "1 2 3 4")
 
 let suite =
     [ "when_empty_string_should_return_none", `Quick, when_empty_string_should_return_none
+    ; "when_wring_string_1_should_return_none", `Quick, when_wrong_string_1_should_return_none
+    ; "when_wring_string_2_should_return_none", `Quick, when_wrong_string_2_should_return_none
+    ; "when_wring_string_3_should_return_none", `Quick, when_wrong_string_3_should_return_none
+    ; "when_wring_string_4_should_return_none", `Quick, when_wrong_string_4_should_return_none
+    ; "when_wring_string_5_should_return_none", `Quick, when_wrong_string_5_should_return_none
+    ; "when_wring_string_6_should_return_none", `Quick, when_wrong_string_6_should_return_none
+    ; "when_wring_string_7_should_return_none", `Quick, when_wrong_string_7_should_return_none
+    ; "when_string_should_return_area", `Quick, when_string_should_return_area
     ]
 
 let () =
